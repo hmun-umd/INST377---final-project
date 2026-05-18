b2427a165897295d506674d3830f54baa67b30cb
Hong Lim Mun <hmun@terpmail.umd.edu>
2026-05-17 20:45:56 -0400

    added: h1

--- index.js
+++ index.js
@@ -33,6 +33,54 @@ app.use((req,res) => {
     res.status(404).sendFile('public/404.html', { root: __dirname });
 });
 
+app.get('/customers', async (req, res) => {
+    console.log('Attempting to get all customers!');
+
+    const { data, error } = await supabase.from('customer').select();
+
+    if(error){
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    } else {
+        console.log('Recieved Data:', data);
+        res.json(data);
+    }   
+});
+
+app.post('/customer', async (req, res) => { //post = add data
+    console.log('Adding Customer');
+    console.log(`Request: ${JSON.stringify(req.body)}`);
+
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const state =  req.body.state;
+
+    if(!isValidStateAbbreviation(state)) {
+        console.log(`State: ${state} is invalid`);
+        res.statusCode = 400;
+        res.json({
+            message: `${state} is not a valid 2 Letter Abbreviation for State`,
+        });
+        return;
+    }
+
+    const { data, error } = await supabase.from('customer').insert({
+        customer_first_name: firstName,
+        customer_last_name: lastName,
+        customer_state: state,
+    })
+    .select();
+
+    if(error) {
+        console.log(`Error: ${error}`);
+        res.statusCode = 500;
+        res.send(error);
+    } else {
+        res.json(data);
+    }
+}); 
+
 app.listen(port, () => {
     console.log(`App is available on port: ${port}`);
 });
